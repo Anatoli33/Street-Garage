76c26046acadfdb3602da235a8f57188920124b5
Anatoli33 <hadjiev1215@gmail.com>
2026-03-27 13:29:02 +0200

    Add route guard

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home.js';
 import { Feed } from './feed/feed.js';
 import { Login } from './login/login.js';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {path: '', component: Home},
-    {path: 'feed', component: Feed},
+    {path: 'feed', component: Feed, canActivate: [authGuard],},
     {path: 'login', component: Login},
 ];
