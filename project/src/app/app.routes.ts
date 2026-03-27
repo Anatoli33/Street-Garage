5a3db40feb6e063d3c3b8ec3b5ce85a1c5d95e48
Anatoli33 <hadjiev1215@gmail.com>
2026-03-27 13:17:37 +0200

    Make the login component work

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home.js';
 import { Feed } from './feed/feed.js';
+import { Login } from './login/login.js';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'feed', component: Feed},
+    {path: 'login', component: Login},
 ];
--- project/src/app/login/login.ts
+++ project/src/app/login/login.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   imports: [],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
 export class Login {
+  constructor(private authService: AuthService){}
 
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
+  onLogin(): void {
+    this.authService.login();
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+  }
 }
