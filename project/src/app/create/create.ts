5e4fa9da6f8c0abfc2f86e8534e5f68b86bd428a
Anatoli33 <hadjiev1215@gmail.com>
2026-04-02 10:31:00 +0300

    Make the class create logic

--- project/src/app/create/create.ts
+++ project/src/app/create/create.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create.html',
   styleUrl: './create.css',
 })
 export class Create {
+   createForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm = this.fb.group({
+      brand: ['', [Validators.required, Validators.minLength(2)]],
+      model: ['', [Validators.required, Validators.minLength(1)]],
+      year: ['', [Validators.required, Validators.min(1900), Validators.max(new Date().getFullYear())]],
+      image: ['', [Validators.required, Validators.pattern(/^https?:\/\/.+/)]],
+      description: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  onCreate() {
+    if (this.createForm.invalid) {
+      this.createForm.markAllAsTouched();
+      return;
+    }
+
+    const carData = this.createForm.value;
+
+    console.log('Car created:', carData);
+
+
+    this.createForm.reset();
+  }
 
 }
