6d3bf855ef094ab1c8ef1e74f2ac9a82c3762514
Anatoli33 <hadjiev1215@gmail.com>
2026-04-09 13:07:46 +0300

    Make Auth Guard

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -9,19 +9,21 @@ import { NotFound } from './not-found/not-found.js';
 // import { carResolver } from './guards/car.resolver.js';
 import { Questions } from './questions/questions';
 import { Answers } from './answers/answers';
+// import { guestGuard } from './guards/auth.guard.js';
+import { authGuard } from './guards/auth.guard.js';
 
 export const routes: Routes = [
-    {path: '', component: Home},
-    {path: 'feed', component: Feed},
-    {path: 'register', component: Register},
-    {path: 'create', component: Create},
-    {path: 'questions', component: Questions},
-    {path: 'answers', component: Answers},
-    //  {
-    // path: 'feed/:id',
-    // component: Details,
-    // resolve: { car: carResolver },
-    // },
-    {path: 'login', component: Login},
-    {path: '**', component: NotFound},
+  { path: '', component: Home },
+  { path: 'feed', component: Feed },
+  { path: 'answers', component: Answers }, 
+
+  
+  { path: 'register', component: Register},
+  { path: 'login', component: Login },
+
+  
+  { path: 'create', component: Create, canActivate: [authGuard] },
+  { path: 'questions', component: Questions, canActivate: [authGuard] },
+
+  { path: '**', component: NotFound },
 ];
---
+++ project/src/app/guards/auth.guard.ts
@@ -0,0 +1,31 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const guestGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const user = authService.currentUser();
+
+  if (user) {
+    router.navigate(['/']);
+    return false;
+  }
+
+  return true; 
+};
+export const authGuard: CanActivateFn = async () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+
+  const user = authService.currentUser();
+
+  if (!user) {
+    router.navigate(['/login']);
+    return false;
+  }
+
+  return true;
+};
